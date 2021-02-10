412192813de8246ba8905e2c37b70f150a6c236e
roman-ts-coax <roman.ts@coaxsoft.com>
2021-02-10 11:47:42 +0200

    27. Add policy to API. Fixed issues with enexisting article during update article. Add a checking on the owner of the сomment

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,15 +1,22 @@
 class ApiController < ActionController::API
-  # TODO: add policy
-  # include Pundit
+  include Pundit
   include I18nLocalise
   respond_to :json
 
   before_action :authenticate_request
   before_action :record_not_found, only: %i[show update destroy]
+  before_action :verify_class, only: %i[index new create]
+  before_action :verify_record, except: %i[index new create]
 
   attr_reader :current_user
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   protected
+
+  def user_not_authorized(*errors)
+    render json: { errors: errors }, status: :forbidden
+  end
 
   def authenticate_request
     @current_user = Api::AuthorizeApiRequest.call(request.headers).result
@@ -26,5 +33,24 @@ class ApiController < ActionController::API
 
   def record_not_found
     render json: { error: 'record not found' }, status: :not_found if record.nil?
+  end
+
+  def verify_record
+    authorize_with_configs(record)
+  end
+
+  def verify_class
+    authorize_with_configs(record_class)
+  end
+
+  def authorize_with_configs(record)
+    return true if policy_class.new(current_user,
+                                    record).send("#{action_name}?")
+
+    raise Pundit::NotAuthorizedError, "not allowed to #{action_name}"
+  end
+
+  def policy_class
+    "#{record_class}Policy".constantize
   end
 end
